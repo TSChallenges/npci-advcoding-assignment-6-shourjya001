98e4bfe175f6f70cddaa915bca0fdf4e06cff7c3
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2025-02-13 10:24:50 +0000

    Completed playground challenge

--- ex2/src/main/java/com/mystore/app/App.java
+++ ex2/src/main/java/com/mystore/app/App.java
@@ -1,16 +1,19 @@
 package com.mystore.app;
 
-public class App {
-    public static void main(String[] arg) {
-        
-        Product p1 = new Product();
-        p1.setId(1);
-        p1.setName("Tumbler");
-        System.out.println(p1);
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-        Product p2 = new Product();
-        p2.setId(2);
-        p2.setName("Bucket");        
+public class App {
+    public static void main(String[] args) {
+        // Load the Spring context
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Get beans from Spring context
+        Product p1 = context.getBean("tumbler", Product.class);
+        Product p2 = context.getBean("bucket", Product.class);
+
+        // Display Products
+        System.out.println(p1);
         System.out.println(p2);
-    }  
+    }
 }
